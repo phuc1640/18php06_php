38687e3b4b8ff670bf7157bc37a4c12fc820f8e2
phuc1640 <tasinhphuc@gmail.com>
2018-11-22 20:15:10 +0700

    thanh cong

--- session13/exercise4.php
+++ session13/exercise4.php
@@ -1,5 +1,5 @@
 <?php
-	$vnd = 2000;
+	$vnd = 200000;
 	$candies = 0;
 	$wrapper = 0;
 	while ($vnd > 0) {
@@ -8,8 +8,12 @@
 		$wrapper++;
 		if ($wrapper == 2) {
 			$candies++;
-			$wrapper = 1;
+			$wrapper--;
 		}
 	}
 	echo $candies;
 ?>
+<!-- BT4: Cho ban 2000 vnđ đi mua kẹo .Biết :
+1 viên kẹo giá 200 vnđ.
+cứ 2 vỏ kẹo đổi được 1 viên.
+Hỏi với 2000 vnđ, ban sẽ mua đc bao nhiêu viên kẹo ?? -->
