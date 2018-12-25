a6dfc02e29be0d4559ec6b85afaaba66e555bc5b
phuc1640 <tasinhphuc@gmail.com>
2018-12-25 18:32:56 +0700

    thanh cong

--- session16/inputFormProduct.php
+++ session16/inputFormProduct.php
@@ -7,17 +7,7 @@
 <body>
 	<?php
 	$tableName = 'products';
-	function connectDb() {
-		$servername = "localhost";
-		$username = "root";
-		$password = "abc123";
-		$dbname = "18php06_php";
-		$conn = new mysqli($servername, $username, $password, $dbname);
-		if ($conn->connect_error) {
-		    die("Connection failed: " . $conn->connect_error);
-		}
-		return $conn;
-	}
+	include 'connectDb.php';
 	function insertProduct($name ,$description, $image, $price, $status, $created) {
 		$message = "";
 		global $tableName;
